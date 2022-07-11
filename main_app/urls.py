7e63aa619953b227a7e04864b5ea9beb8fd18740
Me <104526114+bmather02@users.noreply.github.com>
2022-07-11 16:16:04 -0400

    adds full CRUD for sheets.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('stepsheets/', views.sheet_index, name='index'),
+    path('stepsheets/<int:sheet_id>/', views.sheet_detail, name='detail'),
+    path('stepsheets/submit/', views.SheetCreate.as_view(), name='sheets_create'),
+    path('stepsheets/<int:pk>/update/', views.SheetUpdate.as_view(), name='sheets_update'),
+    path('stepsheets/<int:pk>/delete/', views.SheetDelete.as_view(), name='sheets_delete'),
+    path('choreographers/', views.ChoreoList.as_view(), name='choreo_index'),
+    path('choreographers/<int:pk>', views.ChoreoDetail.as_view(), name='choreo_detail'),
+    path('choreographers/add', views.ChoreoCreate.as_view(), name='choreo_create'),
+    path('choreographers/<int:pk>/update', views.ChoreoDetail.as_view(), name='choreo_detail'),
+    path('stepsheets/<int:sheet_id>/assoc_choreo/<int:toy_id>/', views.assoc_choreo, name='assoc_choreo')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Sheet
+from django.shortcuts import render, redirect
+from .models import Choreographer, Sheet
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -10,3 +13,36 @@ def about(request):
 def sheet_index(request):
     sheets = Sheet.objects.all()
     return render(request, 'sheets/index.html', {'sheets': sheets})
+
+def sheet_detail(request, sheet_id):
+    sheet = Sheet.objects.get(id=sheet_id)
+    return render(request, 'sheets/detail.html', {'sheet': sheet})
+
+class SheetCreate(CreateView):
+    model = Sheet
+    fields = '__all__'
+    success_url = '/stepsheets/'
+
+class SheetUpdate(UpdateView):
+    model = Sheet
+    fields = '__all__'
+
+class SheetDelete(DeleteView):
+    model = Sheet
+    success_url = '/stepsheets/'
+
+class ChoreoList(ListView):
+    model = Choreographer
+    template_name = 'choreographers/index.html'
+
+class ChoreoDetail(DetailView):
+    model = Choreographer
+    template_name = 'choreographers/detail.html'
+
+class ChoreoCreate(CreateView):
+    model = Choreographer
+    fields = '__all__'
+
+def assoc_choreo(request, sheet_id, choreo_id):
+    Sheet.objects.get(id=sheet_id).choreo.add(choreo_id)
+    return redirect('detail', sheet_id=sheet_id)
