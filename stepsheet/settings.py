c7eefd7ce045d7c034535789615a4394038ec49a
Me <104526114+bmather02@users.noreply.github.com>
2022-07-12 15:24:38 -0400

    hides secret key.

--- stepsheet/settings.py
+++ stepsheet/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv, find_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r_a125vdi!%p$)ae-u!%($+rn_m4@m32$g5w^-$af$$&da1yp_'
+load_dotenv(find_dotenv())
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
