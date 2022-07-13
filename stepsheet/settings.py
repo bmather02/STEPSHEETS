23184f238cf468567dc29e8265a4c83ae8106f1b
Me <104526114+bmather02@users.noreply.github.com>
2022-07-13 17:28:56 -0400

    adds secret key back?

--- stepsheet/settings.py
+++ stepsheet/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-r_a125vdi!%p$)ae-u!%($+rn_m4@m32$g5w^-$af$$&da1yp_'
+# os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
